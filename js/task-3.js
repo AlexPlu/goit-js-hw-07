1b78fd9868e02338f33cc6a4f533cab717199e7a
Oleksandr Pluzhnykov <bbynnas@yahoo.com>
2024-01-06 05:18:51 +0200

    goit-js-hw-07

---
+++ js/task-3.js
@@ -0,0 +1,7 @@
+var inputElement = document.getElementById("name-input");
+var outputElement = document.getElementById("name-output");
+
+inputElement.addEventListener("input", function () {
+  var inputValue = inputElement.value.trim();
+  outputElement.textContent = inputValue === "" ? "Anonymous" : inputValue;
+});
